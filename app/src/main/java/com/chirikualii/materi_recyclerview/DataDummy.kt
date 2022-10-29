9e31819cfc4a768641f372f5ed6bd61e81fec3d1
DESKTOP-AL04PS9\Lab-ERP-20 <201210098@student.mercubuana-yogya.ac.id>
2022-10-29 14:54:38 +0700

    first commit

---
+++ app/src/main/java/com/chirikualii/materi_recyclerview/DataDummy.kt
@@ -0,0 +1,49 @@
+package com.chirikualii.materi_recyclerview
+
+object DataDummy  {
+
+    // TODO add data dummy
+    val listProfile = listOf<Profile>(
+        Profile(
+            name = "Bang Chan",
+            phone = "081234567891",
+            image = "https://i.redd.it/i83a2w9vs3a71.jpg"
+        ),
+        Profile(
+            name = "Lee Know",
+            phone = "081234567891" ,
+            image = "https://i.redd.it/i83a2w9vs3a71.jpg"
+        ),
+        Profile(
+            name = "Seo Changbin",
+            phone = "081234567891" ,
+            image = "https://i.redd.it/i83a2w9vs3a71.jpg"
+        ),
+        Profile(
+            name = "Hawang Hyunjin",
+            phone = "081234567891" ,
+            image = "https://i.redd.it/i83a2w9vs3a71.jpg"
+        ),
+        Profile(
+            name = "Han Jisung",
+            phone = "081234567891" ,
+            image = "https://i.redd.it/i83a2w9vs3a71.jpg"
+        ),
+        Profile(
+            name = "Kim Seungmin",
+            phone = "081234567891" ,
+            image = "https://i.redd.it/i83a2w9vs3a71.jpg"
+        ),
+        Profile(
+            name = "Felix",
+            phone = "081234567891" ,
+            image = "https://i.redd.it/i83a2w9vs3a71.jpg"
+        ),
+        Profile(
+            name = "Yang Jeongin",
+            phone = "081234567891" ,
+            image = "https://i.redd.it/i83a2w9vs3a71.jpg"
+        )
+
+    )
+}
